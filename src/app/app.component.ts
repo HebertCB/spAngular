db08f670e33cabb36f9dfdae60a3bf84155ee7c7
HebertCB <hebertcoazaca@gmail.com>
2018-05-21 10:12:14 -0500

    Archivos base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { MatriculaComponent } from './matricula/matricula.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MatriculaComponent } from './matricula/matricula.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MatriculaComponent,
+    ContactoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
